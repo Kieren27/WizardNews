e8f2dacdc7164fb5abd425b2c0ff411d6bf323da
Kieren Enright <kierenenright27@gmail.com>
2023-05-03 21:28:54 -0700

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,51 @@
 const express = require("express");
+const morgan = require("morgan");
+const path = require('path');
+const postBank = require("./postBank");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static(path.join(__dirname, './public')));
+
+app.use(morgan('dev'));
+
+app.get("/", (_req, res) => {
+  const posts = postBank.list();
+
+  const html = 
+  `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`
+
+  res.send(html);
+});
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  res.send(post);
+})
 
 const PORT = 1337;
 
